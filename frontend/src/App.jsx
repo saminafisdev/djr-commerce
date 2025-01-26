b0e7b50fe17062bf2773ea601dee58084ba4752f
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-01-27 00:17:48 +0600

    Add ProductList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router";
 import { Home } from "./components/Home";
 import { MainLayout } from "./layout/MainLayout";
+import { ProductsList } from "./features/products/ProductsList";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route index element={<Home />} />
+          <Route path="/products" element={<ProductsList />} />
         </Route>
       </Routes>
     </>
---
+++ frontend/src/features/products/ProductsList.jsx
@@ -0,0 +1,85 @@
+import { Rating } from "@/components/ui/rating";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Float,
+  Grid,
+  GridItem,
+  Group,
+  IconButton,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+
+import { FiHeart } from "react-icons/fi";
+import { FiShoppingBag } from "react-icons/fi";
+
+const ProductCard = () => {
+  return (
+    <GridItem>
+      <Box>
+        <Center
+          rounded={"md"}
+          position={"relative"}
+          height={250}
+          bg={"gray.50"}
+        >
+          <Image
+            height={200}
+            rounded={"md"}
+            src="https://pngimg.com/uploads/headphones/headphones_PNG7645.png"
+            alt="product name"
+          />
+          <Float offset={8}>
+            <IconButton
+              rounded={"full"}
+              colorPalette={"gray"}
+              variant={"subtle"}
+            >
+              <FiHeart />
+            </IconButton>
+          </Float>
+        </Center>
+        <Box>
+          <Text textStyle={"xl"}>T-shirt for men</Text>
+          <Group>
+            <Rating
+              defaultValue={3}
+              size="sm"
+              colorPalette={"orange"}
+              readOnly
+            />
+            <Text>(300)</Text>
+          </Group>
+          <Text fontWeight={"semibold"}>$20</Text>
+        </Box>
+        <Button variant={"outline"} mt={4} colorPalette={"blue"} width={"full"}>
+          <FiShoppingBag /> Add to Cart
+        </Button>
+      </Box>
+    </GridItem>
+  );
+};
+
+export const ProductsList = () => {
+  return (
+    <Container>
+      <Text fontSize={"2xl"} fontWeight={"medium"} mb={4}>
+        Product List
+      </Text>
+      <Grid templateColumns={"repeat(4, 1fr)"} gap={4}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </Grid>
+    </Container>
+  );
+};
