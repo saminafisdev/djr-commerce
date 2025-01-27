f99b0e299f9e643ee1beabeefea990e72b3c78ed
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-01-27 13:25:12 +0600

    Add Redux Toolkit integration with store and apiSlice setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider as ChakraProvider } from "@/components/ui/provider";
 import { BrowserRouter } from "react-router";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
+    </Provider>
   </StrictMode>
 );
