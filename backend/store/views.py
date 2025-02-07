4807f3e0a15d49a770a24cd9d7aca598c8679695
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-02-07 16:08:27 +0600

    Add delete functionality to CartAPIView for removing cart items

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.request import Request
@@ -90,6 +91,30 @@ class CartAPIView(APIView):
         serializer.is_valid()
         serializer.save()
         return Response(serializer.data)
+
+    def delete(self, request, format=None):
+        item_id = request.data.get("item_id")
+        if not item_id:
+            return Response(
+                {"detail": "Item ID is required."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            cart_item = CartItem.objects.get(pk=item_id)
+
+            if cart_item.cart.customer.user != request.user:
+                return Response(
+                    {"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND
+                )
+
+            cart_item.delete()
+            return Response(
+                {"detail": "Item deleted successfully."},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+
+        except CartItem.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
 
 class CartViewset(
